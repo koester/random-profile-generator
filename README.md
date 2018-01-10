ebb1d8254a54c1770302856aee89cdce169eeab8
koester <koester69@gmail.com>
2018-01-10 15:48:39 +0100

    fixed typos

--- README.md
+++ README.md
@@ -13,9 +13,9 @@ Feel free to clone the repo and play around with the source.
 Clone this repo to your local maschine and install all dependencies:
 
 ```shell
-git clone https://github.com/koester/random-profile-generator.git
-cd random-profile-generator
-npm install
+$ git clone https://github.com/koester/random-profile-generator.git
+$ cd random-profile-generator
+$ npm install
 ```
 
 After that you can run the app on the local development server with `npm run server` or build it with `npm run build`
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /* init */
-import "./component/loading"
-const api = "https://randomuser.me/api/"
-const btn = document.querySelector("button")
-const avatar = document.querySelector("#avatar")
-const name = document.querySelector("#fullname")
-const username = document.querySelector("#username")
-const email = document.querySelector("#email")
-const city = document.querySelector("#city")
+import "./component/loading";
+const api = "https://randomuser.me/api/";
+const btn = document.querySelector("button");
+const avatar = document.querySelector("#avatar");
+const name = document.querySelector("#fullname");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const city = document.querySelector("#city");
 
 /**
  * Checks for response status and
@@ -17,10 +17,10 @@ const city = document.querySelector("#city")
  */
 const handleResponseErrors = res => {
   if (!res.ok) {
-    throw Error(404)
+    throw Error(404);
   }
-  return res
-}
+  return res;
+};
 
 /**
  * Parse the incoming data to JSON
@@ -30,8 +30,8 @@ const handleResponseErrors = res => {
  * @returns {Promise.<Object>}
  */
 const parseJSON = res => {
-  return (res = res.json())
-}
+  return (res = res.json());
+};
 
 /**
  * Get the data from fetched object
@@ -41,22 +41,22 @@ const parseJSON = res => {
  * @returns {Promise.<Object>}
  */
 const updateProfile = res => {
-  let profile = res.results[0]
+  let profile = res.results[0];
   let capitalizeFirstLetter = string => {
-    return string.charAt(0).toUpperCase() + string.slice(1)
-  }
-  let firstName = capitalizeFirstLetter(profile.name.first)
-  let lastName = capitalizeFirstLetter(profile.name.last)
-  let cityName = capitalizeFirstLetter(profile.location.city)
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+  let firstName = capitalizeFirstLetter(profile.name.first);
+  let lastName = capitalizeFirstLetter(profile.name.last);
+  let cityName = capitalizeFirstLetter(profile.location.city);
 
-  avatar.src = profile.picture.large
-  name.innerText = `${firstName} ${lastName}`
-  username.innerText = `@${profile.login.username}`
-  email.innerText = profile.email
-  city.innerText = cityName
+  avatar.src = profile.picture.large;
+  name.innerText = `${firstName} ${lastName}`;
+  username.innerText = `@${profile.login.username}`;
+  email.innerText = profile.email;
+  city.innerText = cityName;
 
-  return res
-}
+  return res;
+};
 
 /**
  * Log any errors to the console
@@ -64,13 +64,13 @@ const updateProfile = res => {
  * @param {Error} error
  */
 const logError = error => {
-  console.log(error)
-}
+  console.log(error);
+};
 
 btn.addEventListener("click", function() {
   fetch(api)
     .then(handleResponseErrors)
     .then(parseJSON)
     .then(updateProfile)
-    .catch(logError)
-})
+    .catch(logError);
+});
